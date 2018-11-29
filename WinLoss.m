b11ef27969b3907020771baa10a6c8e3c4985e35
Brandon Bridges <brndnbrdgs1@gmail.com>
2018-11-29 15:37:09 -0500

    Commented each section of code
    Blackjack.m - added result 3

--- WinLoss.m
+++ WinLoss.m
@@ -1,10 +1,14 @@
 function [game] = WinLoss(p_chips)
 game = 0;
+
+%player loses all chips
 if p_chips < 10
-    disp('\nYou Lost All Your Chips: GAME OVER');
+    fprintf('You Lost All Your Chips: GAME OVER');
     game = 1;
-    elseif p_chips >= 1000000
-      disp('\nYou Won All the Chips: CONGRATS');
+
+%player gets over 100000 chips they win
+    elseif p_chips >= 100000
+      fprintf('\nYou Won All the Chips: CONGRATS');
       game = 2;
     end
 end
