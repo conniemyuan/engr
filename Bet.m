add34f527b53c9573d4932962354558e10670a89
Brandon Bridges <brandonb64446@gmail.com>
2018-12-03 16:51:51 -0500

    fprintf change.

--- Bet.m
+++ Bet.m
@@ -1,14 +1,14 @@
 function [p_bet] = Bet(p_chips)
 
 %if player doesnt enter min bet
-  p_bet = input('Please enter a bet greater than 10 chips: ');
-  while (p_bet < 10 && p_bet > 0) || p_chips<p_bet
+p_bet = input('Please enter a bet >10. To cashout, bet 0: ');
+while (p_bet < 10 && p_bet > 0) || p_chips<p_bet
 
 %if player bet more than they have
-    p_bet=input('Bet must be greater than 10 and less than your total: ');
-  end
+  p_bet=input('Bet must be >10 and less than your total: ');
+end
 
 %if player wants to quit
-  if p_bet<0
-    p_bet = 0;
-  end
+if p_bet<0
+  p_bet = 0;
+end
