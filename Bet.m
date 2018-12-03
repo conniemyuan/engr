7f68be5c9004a3ddc6602ac79f3f4dedc31f9036
Brandon Bridges <brndnbrdgs1@gmail.com>
2018-12-03 15:15:36 -0500

    Revert "Commented each section of code"

--- Bet.m
+++ Bet.m
@@ -1,15 +1,8 @@
 function [p_bet] = Bet(p_chips)
-  fprintf('\n')
-
-%player doesnt enter min bet
-  p_bet = input('\nPlease enter a bet greater than 10 chips: ');
-  while (p_bet < 10 && p_bet > 0) || p_chips<p_bet
-
-%player bet more than they have
+p_bet = input('\nPlease enter a bet greater than 10 chips: ');
+while (p_bet < 10 && p_bet > 0) || p_chips<p_bet
     p_bet=input('\nBet must be greater than 10 and less than your total: ');
-  end
-
-%player wants to quit
-  if p_bet<0
+end
+if p_bet<0
     p_bet = 0;
-  end
+end
